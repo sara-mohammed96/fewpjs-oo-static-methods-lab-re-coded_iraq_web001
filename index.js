66a13a13573c7f1384597f286c90f4e2e8c56a09
sara altaee <sara.korea96@gmail.com>
2020-06-14 00:07:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 class Formatter {
   //add static methods here
+ 
+  static capitalize(string){
+    return string.charAt(0).toUpperCase()+string.slice(1);
+  }
+  
+  static sanitize(string){
+   return string.replace(/[^A-Za-z0-9-'-' ']+/g, '')
+  }
+  
+  static titleize(string){
+    let words = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return this.capitalize(string.split(' ').map(word => words.includes(word) ? word : this.capitalize(word)).join(' '))
+  }
 }
