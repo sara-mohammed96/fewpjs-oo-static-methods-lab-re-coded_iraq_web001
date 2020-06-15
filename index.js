30f06b00c41bd8d25bf40fca6cd12712c344ef0b
sara altaee <sara.korea96@gmail.com>
2020-06-15 20:45:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ class Formatter {
   }
   
   static titleize(string){
+<<<<<<< HEAD
     let words = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
     return this.capitalize(string.split(' ').map(word => words.includes(word) ? word : this.capitalize(word)).join(' '))
+=======
+    return string.split(' ').map(x=>string.replace(/the/-/a/-/but/-/of/-/and/+/g, '') ,this.capitalize(string));
+>>>>>>> 3d53f647d380a598d7e7bad9f86b102c1c1a2ac0
   }
 }
